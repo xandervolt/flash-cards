ad10b385896f05705e9cc927ede824a95d06b522
Alexander <alexander.v.joseph@gmail.com>
2018-12-21 15:31:00 -0700

    everything done except css

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var cardsRouter = require('./routes/cards');
+const cardsRouter = require('./routes/cards');
 
 var app = express();
 
